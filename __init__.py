595a92600fc99bca0025785478b3c1799f216190
Keunbae Park <ppotpo@gmail.com>
2022-05-12 16:19:06 -0400

    wip

--- __init__.py
+++ __init__.py
@@ -9,26 +9,29 @@ __authors__ = "pkboom"
 
 icon = "{}/icon.png".format(path.dirname(__file__))
 
+
 def handleQuery(query):
     if not query.isTriggered or not query.isValid:
         return
-        
+
     items = []
 
     if query.string.strip():
-        sql="""
+        sql = """
 select page, anchor, heading, body, id, page_heading from tailwind_docs 
 where heading like '%{search}%'
 or match (body) against ('{search}' in natural language mode) 
 """
-        mydb = mysql.connector.connect(user='root', password='', host='127.0.0.1', database='docs')
+        mydb = mysql.connector.connect(
+            user='root', password='', host='127.0.0.1', database='docs')
         mycursor = mydb.cursor()
-        mycursor.execute(sql.format(search = query.string))
+        mycursor.execute(sql.format(search=query.string))
         results = mycursor.fetchall()
         mydb.close()
 
         for result in results:
-            url = 'https://tailwindcss.com/docs/{page}{anchor}'.format(page = result[0], anchor = '#'+result[1] if result[1] else '')
+            url = 'https://tailwindcss.com/docs/{page}{anchor}'.format(
+                page=result[0], anchor='#'+result[1] if result[1] else '')
 
             excerpt = ''
 
@@ -41,20 +44,24 @@ or match (body) against ('{search}' in natural language mode)
             if (query.string.lower() not in result[2].lower() and len(result[3]) > 0 and query.string.lower() not in result[3].lower()):
                 continue
 
-            if query.string.lower() in result[3].lower() if result[3] else '': 
+            if query.string.lower() in result[3].lower() if result[3] else '':
                 start = result[3].lower().find(query.string.lower())
                 bodyLength = len(result[3])
 
-                find = result[3][0 if start < 40 else start - 40: bodyLength if start + len(query.string) + 40 > bodyLength else start + len(query.string) + 40]
-                excerpt = (result[5] if result[5] else '') + (' ' if start < 40 else '...') + find + ('' if start + len(query.string) + 40 > bodyLength else '...')
+                find = result[3][
+                    0 if start < 40 else start - 40: bodyLength if start + len(query.string) + 40 > bodyLength else start + len(query.string) + 40
+                ]
+                excerpt = (result[5]+'...' if result[5] else '') + \
+                    find + \
+                    ('' if start + len(query.string) + 40 > bodyLength else '...')
 
             items.append(Item(
                 id=str(result[4]),
                 icon=icon,
-                text = result[2],
-                subtext= excerpt if excerpt else '',
+                text=result[2],
+                subtext=excerpt if excerpt else '',
                 actions=[UrlAction(
-                    text="This action opens tailwind docs.", 
+                    text="This action opens tailwind docs.",
                     url=url
                 )],
             ))
@@ -64,7 +71,7 @@ or match (body) against ('{search}' in natural language mode)
             text="Open Docs",
             subtext="Open tailwindcss.com/docs",
             actions=[UrlAction(
-                text="This action opens tailwind docs.", 
+                text="This action opens tailwind docs.",
                 url="https://tailwindcss.com/docs"
             )],
         ))
